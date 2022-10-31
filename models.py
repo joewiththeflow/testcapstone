3efb7205003364d3e66445d29d159bace43fbfac
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-10-31 20:59:42 +0000

    adding models.py basic config

--- models.py
+++ models.py
@@ -1,1 +1,22 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
+
+
+database_path = 'postgresql://{}/{}'.format('localhost:5432', DB_NAME)
+
+db = SQLAlchemy()
+
+"""
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+"""
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
