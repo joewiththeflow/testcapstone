fd7483f7de8a370c762eb767fa8aa758d61c1e64
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-11-01 10:15:48 +0000

    Basic POST movies and actors completed

--- app.py
+++ app.py
@@ -33,10 +33,72 @@ def create_app(test_config=None):
       }
     )
 
+  # For movies we can allow the Executive Producer to add existing actors during movie creation
+  # No other role can add movies
+  # For this we need to ensure that actors actually exist if we try to add them to a movie
+  # A Casting director can add Actors so we can allow that endpoint to add existing movies to actors
+  @app.route("/movies", methods=["POST"])
+  def create_movie():
+      body = request.get_json()
 
+      new_title = body.get("title", None)
+      new_release_date = body.get("release_date", None)
+      actors = body.get("actors", None)
+      
+      # TODO Some form of check regarding release_date
+
+      new_movie = Movie(title=new_title, release_date=new_release_date)
+
+      # Add existing actors to movie if included
+      if actors:
+        for id in actors:
+          actor = Actor.query.filter(Actor.id == id).one_or_none()
+          if actor:
+            new_movie.actors.append(actor)
+          else:
+            abort(404)
+
+      new_movie.insert()
+      
+      return jsonify(
+        {
+            "success": True,
+            "created": new_movie.id
+        }
+      )
+
+
+  @app.route("/actors", methods=["POST"])
+  def create_actor():
+      body = request.get_json()
+
+      new_name = body.get("name", None)
+      new_age = body.get("age", None)
+      new_gender = body.get("gender", None)
+      movies = body.get("movies", None)
+
+      new_actor = Actor(name=new_name, age=new_age, gender=new_gender)
+
+      # Add existing movies to actor if included
+      if movies:
+        for id in movies:
+          movie = Movie.query.filter(Movie.id == id).one_or_none()
+          if movie:
+            new_actor.movies.append(movie)
+          else:
+            abort(404)
+
+      new_actor.insert()
+      
+      return jsonify(
+        {
+            "success": True,
+            "created": new_actor.id
+        }
+      )
   return app
 
-  
+
 
 app = create_app()
 
