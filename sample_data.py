0c25ffea7f7c0d1dbc9827401a83713da67986c2
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-11-01 16:06:57 +0000

    initial sample data

---
+++ sample_data.py
@@ -0,0 +1,32 @@
+from models import Movie, Actor
+
+
+movies = [
+  Movie(title="Star Wars: Episode 1 - The Phantom Menace", release_date=1999),
+  Movie(title="Star Wars: Episode 2 - Attack of the Clones", release_date=2002),
+  Movie(title="Star Wars: Episode 3 - Revenge of the Sith", release_date=2005),
+  Movie(title="Star Wars: Episode 4 - A New Hope", release_date=1977),
+  Movie(title="Star Wars: Episode 5 - The Empire Strikes Back", release_date=1980),
+  Movie(title="Star Wars: Episode 6 - Return of the Jedi", release_date=1983),
+]
+
+
+
+actors = [
+  Actor(name="Ewan Mcgregor", age=51, gender="male"),
+  Actor(name="Hayden Christensen", age=41, gender="male"),
+  Actor(name="Natalie Portman", age=41, gender="female"),
+  Actor(name="Mark Hamill", age=71, gender="male"),
+  Actor(name="Harrison Ford", age=80, gender="male"),
+]
+
+
+new_movie = Movie(title="Star Wars: Episode 7 - The Force Awakens", release_date=2015)
+
+new_actor = Actor(name="Daisy Ridley", age=30, gender="female")
+
+new_movie_non_existent_actor_id = new_movie.format()
+new_movie_non_existent_actor_id["actors"] = [10000]
+
+new_actor_non_existent_movie_id = new_actor.format()
+new_actor_non_existent_movie_id["movies"] = [10000]
