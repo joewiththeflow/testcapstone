df5b35e06237a5d928bca8bd84e95e64db14d62c
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-11-04 16:30:04 +0000

    Added auth0 info

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://joe@localhost:5432/capstone"
 export TEST_DATABASE_URL="postgresql://joe@localhost:5432/capstone_test"
+export AUTH0_DOMAIN="dev-zk1xgmkt.us.auth0.com"
+export ALGORITHMS=["RS256"]
+export API_AUDIENCE="capstone"
 echo "setup.sh script executed successfully!"
