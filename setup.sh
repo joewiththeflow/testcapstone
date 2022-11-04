9629f9de047f2e923a2a94669c7d89ca42f2664b
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-11-04 15:46:41 +0000

    add database urls

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+export DATABASE_URL="postgresql://joe@localhost:5432/capstone"
+export TEST_DATABASE_URL="postgresql://joe@localhost:5432/capstone_test"
+echo "setup.sh script executed successfully!"
